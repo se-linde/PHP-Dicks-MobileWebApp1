7a6515c272256b577d195dc715e74a7b033f6f47
Dwayne Linde <lindesara@gmail.com>
2016-10-10 17:52:56 -0700

    Changed files inc. CSS

--- includes/header.php
+++ includes/header.php
@@ -23,7 +23,7 @@
 <header> 
         
   <!-- <h2 class="pageid">Home Page</h2> -->
-  <h3><a href="index.php">Sara Dwayne Linde</a></h3>
+  <h3><a href="index.php">Dick's Food Calculator</a></h3>
   <!-- START NAVIGATION (Main) -->
     
     
