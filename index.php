08cb7e539a572dd51012032a1dae427985dd44f7
Dwayne Linde <lindesara@gmail.com>
2016-10-10 17:46:28 -0700

    Skeleton files, including includes and CSS

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include 'includes/header.php';?>    
+
+    
+<h1>Hi. I'm Sara Dwayne Linde.</h1>
+   
+
+    <p>I'm a Web Developer, who is interested in both Front-End and Back-End Development. I have 15 years of experience as a Software Test Engineer, and I think that that gives me a great insight into the development process, that other developers are missing. I am interested in either Full Time or Part Time work.  
+</p>    
+    
+    
+
+
+    <br>Links to my resume, where you can see my experience. It's available in three different formats for your convenience: 
+
+    <br><a href="Resume_LindeDwayneSara_website.pdf" target="_blank">Resume of Sara Dwayne Linde - PDF</a>.
+
+
+    <br><a href="Resume_LindeDwayneSara_website.docx" target="_blank">Resume of Sara Dwayne Linde - DOCX</a>.
+
+
+    <br><a href="Resume_LindeDwayneSara_website.txt" target="_blank">Resume of Sara Dwayne Linde - TXT</a>.
+    
+    <p>A link to my Github Profile, where you can see my code: <a href="https://github.com/se-linde" target="_blank">https://github.com/se-linde</a>.</p>
+
+
+    <p>A link to my Developer's Blog, where you can read about my ideas and experiments: <a href="https://hailzorp.wordpress.com/" target="_blank">https://hailzorp.wordpress.com/</a>.</p>
+    
+
+
+    <p><em>Kitten not included.</em></p>
+    
+  
+    <a href="http://www.clker.com/clipart-260188.html">
+    <img class="desktop" src="images/kitten.jpg" alt="A kitten, mewling wistfully whilst you code.">
+        
+    </a>
+    <br><small>Labeled for reuse.</small>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p><small>I also really, really like kittens.</small></p>
+    
+        
+<?php include 'includes/footer.php';?> 
