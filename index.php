0dfbcfaafe22a088bbab2f0012e73c7765706606
Dwayne Linde <lindesara@gmail.com>
2016-11-19 00:57:48 -0800

    updating with JS changes.

--- index.php
+++ index.php
@@ -3,6 +3,42 @@
     
 <h1>Dick's Food Calculator</h1>
    
+
+<div>
+    <ul>
+        <li><a href=”#”>Menu 1</a>
+            <ul>
+                <li><a href=”#”>Sub menu 1</a></li>
+                <li><a href=”#”>Sub menu 2</a></li>
+            </ul>
+        </li>
+        <li><a href=”#”>Menu 2</a>
+            <ul>
+                <li><a href=”#”>Sub menu 1</a></li>
+                <li><a href=”#”>Sub menu 2</a></li>
+            </ul>
+        </li>
+        <li><a href=”#”>Menu 3</a>
+            <ul>
+                <li><a href=”#”>Sub menu 1</a></li>
+                <li><a href=”#”>Sub menu 2</a></li>
+            </ul>
+        </li>
+        <li><a href=”#”>Menu 4</a>
+            <ul>
+                <li><a href=”#”>Sub menu 1</a></li>
+                <li><a href=”#”>Sub menu 2</a></li>
+            </ul>
+        </li>
+        <li><a href=”#”>Menu 5</a>
+            <ul>
+                <li><a href=”#”>Sub menu 1</a></li>
+                <li><a href=”#”>Sub menu 2</a></li>
+            </ul>
+        </li>
+</ul>
+<div>
+
     
         
 <?php include 'includes/footer.php';?> 
