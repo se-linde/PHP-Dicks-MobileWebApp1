d03332729dbce6e83776494d68fa70daeb85058b
Dwayne Linde <lindesara@gmail.com>
2016-11-28 21:56:35 -0800

    Added menu items to index.php
    added menu items to index.php

--- index.php
+++ index.php
@@ -9,35 +9,85 @@
     <section class="page">
       <ul class="accordion">
         <li>
-          <button class="accordion-control">Food Group 1</button>
+          <button class="accordion-control">Burgers and Fries</button>
           <div class="accordion-panel">
-            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
+              
+            <p></p>
+            <ol>
+                <li><b>Dick's Deluxe</b></li>
+                <li><b>Dick's Special</b></li>
+                <li><b>Cheeseburger</b></li>
+                <li><b>Hamburger</b></li>
+                <li><b>French Fries</b></li>
+            </ol>
+              
+              
+              
+              <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
           </div>
         </li>
         <li>
-          <button class="accordion-control">Food Group 2</button>
+          <button class="accordion-control">Milk Shakes and Drinks</button>
           <div class="accordion-panel">
+              
+            <p></p>
+            <ol>
+                <li><b>Shake (Chocolate/Vanilla/Strawberry)</b></li>
+                <li><b>Float</b></li>
+                <li><b>Milk</b></li>
+                <li><b>Coffee</b></li>
+            </ol>
+            
+              
            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
           </div>
         </li>
         <li>
-          <button class="accordion-control">Food Group 3</button>
+          <button class="accordion-control">Sodas</button>
           <div class="accordion-panel">
+              
+            <p></p>
+            <ol>
+                <li><b>Small</b></li>
+                <li><b>Medium</b></li>
+                <li><b>Large</b></li>
+            </ol>
+              
             <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
           </div>
         </li>
           <li>
-          <button class="accordion-control">Food Group 4</button>
+          <button class="accordion-control">Old Fashioned Ice Cream</button>
           <div class="accordion-panel">
+              
+            <p></p>
+            <ol>
+                <li><b>Kids Cones</b></li>
+                <li><b>Single Cones</b></li>
+                <li><b>Double Scoop</b></li>
+                <li><b>Sundaes</b></li>
+            </ol>
+              
             <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
           </div>
         </li>
+          
           <li>
-          <button class="accordion-control">Food Group 5</button>
+          <button class="accordion-control">Condiments</button>
           <div class="accordion-panel">
+              
+            <p></p>
+            <ol>
+                <li><b>Onions</b></li>
+                <li><b>Ketchup</b></li>
+                <li><b>Tartar</b></li>
+                <li><b>Mustard</b></li>
+            </ol>
+              
             <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
           </div>
         </li>
+          
       </ul>
     </section>
 
