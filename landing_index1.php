d7b3554b9ae254eb8ae0cba448b5aa12e4cf054b
Dwayne Linde <lindesara@gmail.com>
2016-11-30 18:31:07 -0800

    Updated files.
    Updated files.

--- landing_index1.php
+++ landing_index1.php
@@ -1,125 +1,30 @@
 <?php include 'includes/header.php';?>    
 
+
+<section class="landing">
+
+<p></p>
+    
 <small>This is not endorsed or sponsored by the Dick's Drive-In Restaurant chain; this is an independent project meant as an academic exercise.</small>
 
 <p></p>
 
-<section class="landing">
+<p>Please pick the city your Dick's Drive-In Location is in: </p>    
 
- <h3>Seattle</h3>
-  <p>Seattle is the center of the Greater Tokyo Area,
-  and the most populous metropolitan area in the world.</p>
-
- <h3>Edmonds</h3>
-  <p>Edmonds is the center of the Greater Tokyo Area,
-  and the most populous metropolitan area in the world.</p>
+<p>If you're on Highway 99, pick 'Edmonds'. Otherwise, pick 'Seattle'.</p>
     
-<h3><a href="index.php">Montreal</a></h3>    
+<h3><a href="index.php">Seattle</a></h3>    
 
-<h3><a href="index.php">Toronto</a></h3>
+<h3><a href="index.php">Edmonds</a></h3>
+
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+
     
 </section>
 
-
-
-    <section class="page">
-      <ul class="accordion">
-        <li>
-          <button class="accordion-control">Burgers and Fries</button>
-          <div class="accordion-panel">
-              
-            <p></p>
-            <ol>
-                <li><b>Dick's Deluxe</b> - $3.10</li>
-                <li><b>Dick's Special</b> - $2.00</li>
-                <li><b>Cheeseburger</b> - $1.75</li>
-                <li><b>Hamburger</b> - $1.40</li>
-                <li><b>French Fries</b> - $1.75</li>
-            </ol>
-              
-              
-              
-              <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
-          </div>
-        </li>
-        <li>
-          <button class="accordion-control">Milk Shakes and Drinks</button>
-          <div class="accordion-panel">
-              
-            <p></p>
-            <ol>
-                <li><b>Shake</b> - $2.50</li>
-                <li><b>Hot Chocolate</b> - $1.25</li>
-                <li><b>2% Milk</b> - $1.25</li>
-                <li><b>Coffee</b> - $1.25</li>
-            </ol>
-            
-              
-           <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
-          </div>
-        </li>
-        <li>
-          <button class="accordion-control">Sodas</button>
-          <div class="accordion-panel">
-              
-            <p></p>
-            <ol>
-                <li><b>Small</b> - $1.40</li>
-                <li><b>Medium</b> - $1.70</li>
-                <li><b>Large</b> - $1.90</li>
-            </ol>
-              
-            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
-          </div>
-        </li>
-          <li>
-          <button class="accordion-control">Old Fashioned Ice Cream</button>
-          <div class="accordion-panel">
-              
-            <p></p>
-            <ol>
-                <li><b>Kids Cones</b> - $1.30</li>
-                <li><b>Single Cone</b> - $1.90</li>
-                <li><b>Double Cone</b> - $2.80</li>
-                <li><b>Sundaes</b> - $2.30</li>
-                <li><b>Root Beer Float</b> - $2.50</li>
-            </ol>
-              
-            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
-          </div>
-        </li>
-          
-          <li>
-          <button class="accordion-control">Condiments</button>
-          <div class="accordion-panel">
-              
-            <p></p>
-            <ol>
-                <li><b>Onions</b> - $0.05</li>
-                <li><b>Ketchup</b> - $0.05</li>
-                <li><b>Tartar</b> - $0.05</li>
-                <li><b>Mustard</b> - $0.05</li>
-            </ol>
-              
-            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</strong> Sed dapibus ac purus ut dapibus. Maecenas in est augue. Pellentesque euismod id mi nec efficitur. Pellentesque pellentesque lectus in sagittis pulvinar. Fusce gravida magna a aliquet lobortis. Sed blandit porta leo ac maximus. Sed consequat euismod ligula pharetra pulvinar. Aliquam placerat venenatis massa, sit amet aliquet quam aliquet eget. Aenean vel lorem sit amet ex dapibus gravida sit amet sit amet tortor. Nam facilisis lacus nulla, id feugiat est venenatis ac. Duis vulputate dui justo, ut commodo metus commodo vitae.</p> 
-          </div>
-        </li>
-          
-      </ul>
-        
-        <p></p>
-            
-        <h4>Subtotal: $</h4>
-        
-        <h4>Tax: $</h4>
-        
-        <h2>Total: $</h2>
-        
-    </section>
-
-  
-    <script src="js/jquery-1.11.0.min.js"></script>
-    <script src="js/accordion2.js"></script>
-
-        
 <?php include 'includes/footer.php';?> 
